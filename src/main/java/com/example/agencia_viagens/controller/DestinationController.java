40e4f19e8fc3c7c7f1f413e62bf706cc1df2606f
Furlan <furlanluisgustavo03@gmail.com>
2026-08-11 21:41:40 -0300

    feat: adiciona o post

--- src/main/java/com/example/agencia_viagens/controller/DestinationController.java
+++ src/main/java/com/example/agencia_viagens/controller/DestinationController.java
@@ -1,4 +1,26 @@
 package com.example.agencia_viagens.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.agencia_viagens.entity.Destination;
+import com.example.agencia_viagens.service.DestinationService;
+
+@RestController
+@RequestMapping("/api/destinos")
 public class DestinationController {
+
+    @Autowired
+    private DestinationService destinationService;
+
+    @PostMapping
+    public ResponseEntity<Destination> cadastrar(@RequestBody Destination destino) {
+        Destination salvo = destinationService.save(destino);
+        return new ResponseEntity<Destination>(salvo, HttpStatus.CREATED);
+    }
 }
--- src/main/java/com/example/agencia_viagens/service/DestinationService.java
+++ src/main/java/com/example/agencia_viagens/service/DestinationService.java
@@ -1,4 +1,18 @@
 package com.example.agencia_viagens.service;
 
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Service;
+
+import com.example.agencia_viagens.entity.Destination;
+
+@Service
 public class DestinationService {
+
+    private ArrayList<Destination> destinos = new ArrayList<Destination>();
+
+    public Destination save(Destination destino) {
+        destinos.add(destino);
+        return destino;
+    }
 }
